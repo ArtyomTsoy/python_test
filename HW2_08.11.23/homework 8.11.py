8fc38f6e1e3c9a96506205693f1ee9c13437f175
ArtyomTsoy <artiomkatsoy@gmail.com>
2023-11-09 17:28:30 +0600

    add to main

---
+++ HW2_08.11.23/homework 8.11.py
@@ -0,0 +1,39 @@
+#                                            NUMBER1
+# name = input("ведите ваше имя: ")
+# surname = input("Введите вашу фамилию: ")
+# patronymic = input("Введите ваше отчество: ")
+# age = (input("Введите ваш возраст: ")
+#
+# print("Здравствуйте! Меня зовут", name, surname, patronymic + ". Мне", age," и я хочу у вас работать.")
+
+
+#                                            NUMBER2
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+#
+# целая_часть = a // b
+# остаток = a % b
+#
+# print(f"Целая часть от деления {целая_часть} и остаток {остаток}.")
+
+
+#                                            NUMBER3
+# str1 = input("Введите данные 1: ")
+# str2 = input("Введите данные 2: ")
+#
+# if str1 == str2:
+#     print(True)
+# else:
+#     print(False)
+
+
+#                                            NUMBER4
+# MULTIPLE_VAL = 6.4
+# M = float(input("Введите число = "))
+# res1 = M * MULTIPLE_VAL
+# res2 = int(res1)
+# res3 = str(res1)
+#
+# print(res1)
+# print(res2)
+# print(res3)
