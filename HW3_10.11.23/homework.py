2705ec62c80ef7c83b0227d0a10db8ee282a1fc6
ArtyomTsoy <artiomkatsoy@gmail.com>
2023-11-13 18:18:03 +0600

    add to main

---
+++ HW3_10.11.23/homework.py
@@ -0,0 +1,34 @@
+#        number1
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = int(input("c = "))
+
+# if a + b > c and b + c > a and a + c > b:
+#     print("True")
+# else:
+#     print("False")
+
+#      number2
+# a = int(input("Введите число: "))
+#
+# res = a % 2 == 0
+# print(res)
+
+#        number3
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = int(input("c = "))
+#
+# if a + b > c:
+#     print("True")
+# else:
+#     print("False")
+
+#         number4
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+#
+# if a > b:
+#     print("True")
+# else:
+#     print("False")
