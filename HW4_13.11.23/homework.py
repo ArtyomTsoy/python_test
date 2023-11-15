ade3ac8680a70b00612d183152933a9b8751888b
ArtyomTsoy <artiomkatsoy@gmail.com>
2023-11-15 14:59:22 +0600

    add to main

---
+++ HW4_13.11.23/homework.py
@@ -0,0 +1,54 @@
+#      Условный оператор. number1
+# login = "user"
+# password = "qwerty"
+#
+# a = input("Введите логин: ")
+# b = input("Введите пароль: ")
+#
+# if a == login and b == password:
+#     print("Authentication completed")
+# else:
+#     print("Invalid Login or Password")
+
+#     Условный оператор. number2
+# money = int(input("Введите сумму в тенге: "))
+#
+# usd = 420
+# eur = 510
+# rub = 5.8
+# print("Выберите валюту: ")
+# print("[1]USD")
+# print("[2]EUR")
+# print("[3]RUB")
+#
+# choice = int(input())
+#
+# if choice == 1:
+#     print(money / usd, "USD")
+# elif choice == 2:
+#     print(money / eur, "EUR")
+# elif choice == 3:
+#     print(money / rub, "RUB")
+# else:
+#     print("Такого выбора на данный момент не существует")
+
+#     Блоки в python. Домашнее задание
+# m = int(input("Зарплата Вани: "))
+# p = int(input("Стоимость пиццы: "))
+# s = int(input("Стоимость подписки: "))
+#
+# if (p + s) <= m:
+#     print("Да")
+# else:
+#     print("Нет")
+
+#     Условия в Python. Домашнее задание
+# ticket = input("Введите номер билета(6 цифр): ")
+#
+# a = int(ticket[0]) + int(ticket[1]) + int(ticket[2])
+# b = int(ticket[3]) + int(ticket[4]) + int(ticket[5])
+#
+# if a == b:
+#     print("Счастливый")
+# else:
+#     print("Обычный")
