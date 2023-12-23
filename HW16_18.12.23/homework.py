e044b5f20c58bccc863c667ead4c55bcb37330e0
ArtyomTsoy <artiomkatsoy@gmail.com>
2023-12-23 22:12:42 +0600

    add to main

---
+++ HW16_18.12.23/homework.py
@@ -0,0 +1,20 @@
+#        number2
+# def plus_two(number):
+#     try:
+#         result = 2 + number
+#         print(result)
+#     except TypeError:
+#         print("Нужный тип данных число")
+#
+# plus_two(7)
+# plus_two('zombie')
+
+#       number3
+# my_list = [0, 1, 2, 3, 4, 5]
+#
+# try:
+#     index = 10
+#     value = my_list[index]
+#     print(index, value)
+# except IndexError:
+#     print("Индекс выходит за границы")
